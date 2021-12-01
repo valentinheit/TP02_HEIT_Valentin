c76996025d5f67523b26eb4b2ae836bac50142c7
Valentin <heit.valentin@icloud.com>
2021-12-01 19:41:04 +0100

    [fix] (server)

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-function requireHTTPS(req, res, next) {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
-    return res.redirect("https://" + req.get("host") + req.url);
-  }
-  next();
-}
 const express = require("express");
+const path = require("path");
 const app = express();
-app.use(requireHTTPS);
-
-app.use(express.static("./dist/<name-on-package.json>"));
+app.use(express.static(__dirname + "/dist/<app-name>"));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/<app-name>/index.html"));
+});
 app.listen(process.env.PORT || 8080);
