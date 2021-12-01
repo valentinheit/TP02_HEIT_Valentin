f029ea3f83c4fd2183550801d2cf5e043b727d42
Valentin <heit.valentin@icloud.com>
2021-12-01 19:04:20 +0100

    [feat] (server) express

---
+++ server.js
@@ -0,0 +1,13 @@
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get("x-forwarded-proto") !== "https") {
+    return res.redirect("https://" + req.get("host") + req.url);
+  }
+  next();
+}
+const express = require("express");
+const app = express();
+app.use(requireHTTPS);
+
+app.use(express.static("./dist/<name-on-package.json>"));
+app.listen(process.env.PORT || 8080);
