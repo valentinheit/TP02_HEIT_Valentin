a2f0679ba7f57a8c05a3df2317f62f2f5a5c9d23
Valentin Heit <heit.valentin@icloud.com>
2021-12-06 20:36:05 +0100

    [feat] (form) : valid verifications
    Verifications about phone, mail, passwords...

--- src/app/saisie-client/saisie-client.component.ts
+++ src/app/saisie-client/saisie-client.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-saisie-client',
@@ -11,16 +12,23 @@ export class SaisieClientComponent implements OnInit {
 
   submitted: boolean = false;
   nom: string = '';
+  nomChanged: boolean = false;
   prenom: string = '';
+  prenomChanged: boolean = false;
   adresse: string = '';
   cp: number = 0;
   ville: string = '';
   tel: string = '';
+  telChanged: boolean = false;
   email: string = '';
+  emailChanged: boolean = false;
   civilite: string = '';
   login: string = '';
+  loginChanged: boolean = false;
   password: string = '';
+  passwordChanged: boolean = false;
   confirmPassword: string = '';
+  confirmPasswordChanged: boolean = false;
   pays: string = '';
   ngOnInit(): void {}
 
@@ -28,6 +36,20 @@ export class SaisieClientComponent implements OnInit {
     return this.isValid;
   }
 
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+
+  isEmailValid() {
+    console.log(this.email);
+    if (this.email) {
+      const regexp = new RegExp('^[^@s]+@[^@s]+.[^@s]+$');
+      if (regexp.test(this.email)) {
+        return true;
+      }
+    }
+    return false;
+  }
   showRecap() {
     if (
       this.nom &&
